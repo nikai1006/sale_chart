1a1ae16bde5dd0f9afb384d29202ba1f953dc460
nikai <keni1006@163.com>
2019-07-06 16:09:49 +0800

    add upload

--- chart/views/views.py
+++ chart/views/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    dates = tuple([i for i in range(1, 13)])
+    return render(request, 'chart/home.html', {'dates': dates})
