39e1751f00cb333fa3332d8085ed10dbc11e6a38
nikai <keni1006@163.com>
2019-07-08 02:08:57 +0800

    echarts

--- chart/views/data_api.py
+++ chart/views/data_api.py
@@ -29,7 +29,10 @@ class ChartData(APIView):
         month = post.get('date')
         print('province=%s, city=%s, month=%s' % (province, city, month))
         bar = Bar()
+        x_axis = []
+        y_axis = []
         if province == 'all':
+            series_name = '全省销售额'
             if month == 'all':
                 totals = runquery(
                     """SELECT SUM(money) AS total, province FROM t_sale_info WHERE `month` in (1,2,3,4,5,6) GROUP BY province ORDER BY total DESC;""")
@@ -39,21 +42,39 @@ class ChartData(APIView):
                     """SELECT SUM(money) AS total, province FROM t_sale_info WHERE `month` =""" + str(
                         month) + """ GROUP BY province ORDER BY total DESC;""")
                 title = str(month) + '月份各省销售情况'
-            x_axis = []
-            y_axis = []
             for total in totals:
                 x_axis.append(total.get('province'))
                 y_axis.append(total.get('total'))
-            bar.add_xaxis(x_axis)
-            bar.add_yaxis(series_name='全省销售额', yaxis_data=y_axis)
-            bar.set_global_opts(title_opts=opts.TitleOpts(title=title))
-
         else:
             if city == 'all':
-                pass
+                if month == 'all':
+                    series_name = province + '省月销售额'
+                    totals = runquery("""SELECT SUM(money) AS total, month FROM t_sale_info WHERE province = '""" + str(
+                        province) + """' GROUP BY `month`;""")
+                    title = province + '省本年度各月销售情况'
+                    for total in totals:
+                        x_axis.append(total.get('month'))
+                        y_axis.append(total.get('total'))
+                else:
+                    series_name = month + '月市销售额'
+                    totals = runquery(
+                        """SELECT money,city FROM t_sale_info WHERE province='""" + province + """' AND `month` = """ + str(
+                            month) + """ ORDER BY money DESC;""")
+                    title = province + '省' + str(month) + '月份各市销售情况'
+                    for total in totals:
+                        x_axis.append(total.get('city'))
+                        y_axis.append(total.get('money'))
             else:
-                pass
-
+                totals = runquery("""SELECT money,month FROM t_sale_info WHERE city = '""" + city + """';""")
+                series_name = province + '省' + city + '市月销售额'
+                title = province + '省' + city + '市各月销售情况'
+                for total in totals:
+                    x_axis.append(total.get('month'))
+                    y_axis.append(total.get('money'))
+        # 绘图
+        bar.add_xaxis(x_axis)
+        bar.add_yaxis(series_name=series_name, yaxis_data=y_axis)
+        bar.set_global_opts(title_opts=opts.TitleOpts(title=title))
         return response_success(data=bar.dump_options())
 
 
