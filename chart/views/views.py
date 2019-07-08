1c22adf486dd5b5f627227c7d7e2c531e74a0295
nikai <854324946@qq.com>
2019-07-08 10:36:59 +0800

    fix bugs

--- chart/views/views.py
+++ chart/views/views.py
@@ -5,7 +5,10 @@ from sale_chart.common.sql_excuter import *
 
 # Create your views here.
 def home(request):
-    dates = tuple([i for i in range(1, 13)])
+    dates = []
+    months = runquery("""SELECT DISTINCT month FROM t_sale_info;""")
+    for month in months:
+        dates.append(month.get('month'))
     provinces = runquery("""SELECT DISTINCT province FROM t_sale_info WHERE province IS NOT NULL && province != '';""")
     prvs = []
     for province in provinces:
